aa803906aa542ac7f60feaace8228b94df2995d7
Josh Chi <joshuachigozie26@gmail.com>
2026-05-07 11:18:42 +0100

    updated 3

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,6 @@
 export const API_BASE = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';
 
-export async function apiFetch(input: RequestInfo | string, init?: RequestInit): Promise<Response> {
+export async function apiFetch(input: RequestInfo | string, init?: Omit<RequestInit, 'body'> & { body?: any }): Promise<Response> {
   let url: RequestInfo = input;
 
   if (typeof input === 'string') {
